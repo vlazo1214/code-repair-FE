6ee00092b4715aeaf1b3f1b032bda6d4ec320b9c
vlazo1214 <vincentl1214@yahoo.com>
2025-02-21 13:47:27 -0500

    test

--- components/front_page.py
+++ components/front_page.py
@@ -35,6 +35,7 @@ def create_full_ui():
         # Front Page UI
         with gr.Column(visible=True) as front_page:
             gr.Markdown("# Welcome to Code Repair with LLMs!")
+            gr.Markdown("# Welcome to Code Repair with LLMs!")
             gr.Markdown("## Upload your files/codebase. All steps of the pipeline will be executed on the initial run.")
 
             file_input = gr.Files(
